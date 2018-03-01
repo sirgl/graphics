2fb1280d49f6ec75f995bee7800a932e6ce37e4a
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2018-03-01 12:51:52 +0700

    simplify

--- src/main/kotlin/sirgl/graphics/filter/hsv/HSVFilterPanel.kt
+++ src/main/kotlin/sirgl/graphics/filter/hsv/HSVFilterPanel.kt
@@ -1,6 +1,7 @@
 package sirgl.graphics.filter.hsv
 
 import sirgl.graphics.components.ObservableSlider
+import sirgl.graphics.components.SplitPanel
 import sirgl.graphics.observable.Observable
 import sirgl.graphics.observable.transmitTo
 import java.awt.Dimension
@@ -10,9 +11,9 @@ import javax.swing.JLabel
 import javax.swing.JPanel
 
 class HSVFilterPanel(
-        private val hSliderPosition: Observable<Int>,
-        private val sSliderPosition: Observable<Int>,
-        private val vSliderPosition: Observable<Int>
+        hSliderPosition: Observable<Int>,
+        sSliderPosition: Observable<Int>,
+        vSliderPosition: Observable<Int>
 ) : JPanel() {
 
     init {
@@ -28,23 +29,11 @@ class HSVFilterPanel(
         add(slider("S", sSliderPosition), c)
         c.gridy = 2
         add(slider("V", vSliderPosition), c)
-
     }
 
     private fun slider(name: String, destObservable: Observable<Int>): JPanel {
-        val c = GridBagConstraints()
-        val panel = JPanel(GridBagLayout())
-        c.gridx = 0
-        c.gridy = 0
-        c.weightx = 1.0
-        c.weighty = 1.0
-        panel.add(JLabel(name))
-
-        c.gridx = 1
-        c.weighty = 5.0
         val slider = ObservableSlider()
         slider.observable.transmitTo(destObservable)
-        panel.add(slider, c)
-        return panel
+        return SplitPanel(JLabel(name), slider, 1.0, 5.0)
     }
 }
